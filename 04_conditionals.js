23b063771c7cd09d2fa130db2078dd7245cad2ca
mccarthygalaxy <124395340+mccarthygalaxy@users.noreply.github.com>
2023-05-25 21:02:15 -0400

    conditionals notes updated

--- 04_conditionals.js
+++ 04_conditionals.js
@@ -54,3 +54,19 @@ if(weather < 70 || rain) {
 } else {
     console.log('Going to be nice.');
 }
+
+/*
+Use this string: let name = 'sAmwISe';
+
+Bronze:
+    Write an if else statement that looks at the first letter of a string. If it is uppercase, console.log the string;
+    If it is not console.log 'hey, this isn't written correctly'
+    
+Silver:
+    If the first letter of a string is uppercase, only console.log that letter
+    if it is not, console.log the rest of the string Without the first letter, and change them to lowercase
+    
+Gold:
+    If the first letter of a string is uppercase, console.log that first letter of a string plus the rest of the string to lowercase.
+    If it is not, console.log the first letter of the string to uppercase plus the rest of the string to lowercase.
+*/
