52a34d79c57d468359f6f8aa18b056d54c696955
mccarthygalaxy <124395340+mccarthygalaxy@users.noreply.github.com>
2023-05-30 21:28:41 -0400

    lecture notes update, also made separate file for Age Challenge Ternary

--- 04_conditionals.js
+++ 04_conditionals.js
@@ -71,3 +71,169 @@ Bronze:
     If it is not, console.log the first letter of the string to uppercase plus the rest of the string to lowercase.
 */
 
+// let grade = 88; // test 1
+// let grade = 73; // test 2 
+
+let grade = 0 // 
+if(grade >= 75){
+console.log("passing");
+} else if(grade>=69) {
+    console.log("please see teacher");
+} else {
+    console.log("failing");
+}
+
+// Once a condition is met - it runs the code block.
+
+/*
+    Look up the format for an Else If statement
+    Set a variable of age and give it a number of your choice
+    Create an else if statement that checks the following:
+    If the age is 17 or younger, console.log 'Sorry, you're too young to do anything.'
+    If the age is at least 18, console.log 'You can vote!'
+    If the age is at least 21, console.log 'You can drink!'
+    If the age is at least 25, console.log 'You can rent a car!'
+*/
+
+let age = 32;
+
+if (age <= 17) {
+    console.log("Sorry, you're too young to do anything.");
+} else if (age >= 25) {
+    console.log("You can rent a car!");
+} else if (age >= 21) {
+    console.log("You can drink!");
+// } else (age >= 18) {
+//     console.log("You can vote!");    
+// }
+} else {
+    console.log("You can vote.");
+}
+
+
+//! SWITCH
+/* 
+    Keywords:
+    - switch
+    - case
+    - break
+    - default
+
+    Frame:
+
+    switch(expression) {
+        case :
+            break;
+        default
+    }
+*/
+
+let officeCharacter = "Kevin";
+
+switch (officeCharacter) {
+    case "Michael" :
+        console.log("My mind is going a mile an hour.");
+        break;
+    case "Dwight" :
+        console.log("Perfectenschlag.");
+        break;
+    case "Jim" :
+        console.log("Bears. Beets. Battlestar Galactica.");
+        break;
+    default :
+        // console.log("I'm sorry but do I know you?");
+        // console.log("I'm sorry, Pam, but do I know you?");
+        console.log(`I'm sorry, ${officeCharacter}, but do I know you?`);// String Interpolation -aka- Template Literal
+}
+
+/*
+    Write a dessert menu;
+    If the dessert is pie, console.log 'Pie, pie, me oh my!'
+    If the dessert is cake, console.log 'Cake is great!'
+    If the dessert is ice cream, console.log 'I scream for ice cream!'
+    Otherwise, have your switch statement console.log 'Not on the menu.'
+*/
+
+// let dessert = "cotton candy";
+let dessert = "ice cream";
+
+switch (dessert) {
+    case "pie" :
+        console.log("Pie, pie, me oh my!");
+        break;
+    case "cake" :
+        console.log("Cake is great!");
+        break;
+    case "ice cream" :
+        console.log("I scream for ice cream!");
+        break;
+    default :
+        console.log("Not on the menu.");       
+}
+
+let num = -5;
+
+switch (true) {
+    case (num < 0 && num > -10):
+        console.log("case 1 ran");
+        break;
+    case (num < 0):
+        console.log("case 2 ran");
+        break;
+    default:
+        console.log("did not work");
+        break;
+}
+
+//! TERNARY
+let number = 6;
+
+//* If/Else
+if (number > 0) {
+    console.log("Yes");
+} else {
+    console.log("No");
+}
+
+number > 0 ? console.log("Yes") : console.log("No");
+(number > 0) ? console.log("Yes") : console.log("No");
+
+// Ternaries require an "else" to the "if" statement.
+
+//* Else/If
+number = 6;
+
+if (number === 0) {
+    console.log("Hello");
+} else if (number < 0) {
+    console.log("Hi");
+} else {
+    console.log("Goodbye");
+}
+
+number === 0 ? console.log("Hello") : number < 0 ? console.log("Hi") :
+console.log("Goodbye");
+// Same as this but cleaner look:
+number === 0 ? 
+    console.log("Hello") : 
+    number < 0 ? 
+    console.log("Hi") :
+    console.log("Goodbye");
+
+/*
+Write a ternary that does the same thing as the age challenge
+
+Age Challenge (if else):
+
+    let age = 30;
+    if (age >= 25) {
+        console.log("Yay! You can rent a car!");
+    } else if (age >= 21) {
+        console.log("Yay! You can drink!");
+    } else if (age >= 18) {
+        console.log("Yay! You can vote!");
+    } else {
+        console.log("Sorry, you're too young to do anything fun.");
+    }
+*/
+
